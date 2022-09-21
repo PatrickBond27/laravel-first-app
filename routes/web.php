bf9a5a3993033f58032cca9e8fb88ca0cfdff6aa
PatrickBond27 <N00201161@iadt.ie>
2022-09-21 15:09:58 +0100

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home', [ 'name' => "Patrick"]);
 });
+
+Route::get('/welcome', function () {
+    return view('welcome', [ 'name' => "Jason"]);
+});
